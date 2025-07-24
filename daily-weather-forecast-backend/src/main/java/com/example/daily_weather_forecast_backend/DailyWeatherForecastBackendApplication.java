5f44b9199b9704e0099265fdb293108d984ecce2
pghgh <68028868+pghgh@users.noreply.github.com>
2025-07-24 10:57:14 +0200

    used spring initializr in order to recreate configurations for the backend project

---
+++ daily-weather-forecast-backend/src/main/java/com/example/daily_weather_forecast_backend/DailyWeatherForecastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.daily_weather_forecast_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyWeatherForecastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyWeatherForecastBackendApplication.class, args);
+	}
+
+}
